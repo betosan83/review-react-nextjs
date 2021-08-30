722d6c8636edf62b85c7a281fb7716fc58969d54
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-08-30 07:25:17 -0300

    First commit!

---
+++ pages/basics/jsx4.jsx
@@ -0,0 +1,10 @@
+export default function jsx4() {
+    const subtitle = "Subtitle"
+    return (
+        <div>
+            <h1>Integration JS and JSX</h1>
+            <h2>{subtitle}</h2>
+            <h3>{Math.max(1, 3)}</h3>
+        </div>
+    )
+}
